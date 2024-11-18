51c2b0e1d4f0386b084402cf5e3f2ded018d6112
Spoonbadger <craig.adam.morley@gmail.com>
2024-11-18 17:46:25 +0000

    File: port created.

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // This binds the server to all network interfaces
+    port: process.env.PORT || 3000,  // Use the port provided by Render, default to 3000 if not available
+  },
 })
