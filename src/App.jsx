72f527db8ebb1b6fa3872608fad63c39d8801272
Spoonbadger <craig.adam.morley@gmail.com>
2024-12-02 15:30:00 -0800

    File: background loading faster update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './static/app.css'
 import './static/fonts.css'
 import './static/animation.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { Routes, Route, useLocation } from 'react-router-dom'
 import HomeView from './components/home/HomeView'
 import AboutView from './components/about/AboutView'
